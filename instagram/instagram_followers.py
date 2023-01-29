e41d211d888dd78b47074e889dcb97a0ff0a71ab
Stanojoski02 <97593843+Stanojoski02@users.noreply.github.com>
2023-01-29 02:34:18 +0100

    Update instagram_followers.py

--- instagram/instagram_followers.py
+++ instagram/instagram_followers.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import time
 
 PATH = 'C:\\Users\Pc4all\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe'
@@ -17,44 +16,60 @@ username.send_keys('')
 password.send_keys('')
 password.send_keys(Keys.ENTER)
 
-time.sleep(3)
+time.sleep(7)
 driver.get("https://www.instagram.com/stanojoski_/?next=%2F")
 time.sleep(5)
 
-followers = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[3]/a')
+followers = driver.find_element(
+    By.XPATH,
+    '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[3]/a'
+)
 followers.click()
-time.sleep(3)
+
+time.sleep(1)
 go_on = True
 el = []
+
 t = 0
-while go_on:
-    try:
-        if t == 1:
-            go_on = False
-        else:
-            t = 1
-        time.sleep(3)
-        command = """
+m = 0
+command = """
         _aano = document.querySelector("._aano");
         _aano.scrollTo(0, _aano.scrollHeight);
         var height = _aano.scrollHeight;
         return height;
         """
-        driver.execute_script(command)
-        time.sleep(5)
-        bojan = 0
+time.sleep(5)
+driver.execute_script(command)
+while go_on:
+    try:
+        time.sleep(2)
+        son = driver.execute_script(command)
+        time.sleep(2)
+        sayfaSonu = driver.execute_script(command)
+        if son == sayfaSonu:
+            go_on = False
         elements = driver.find_elements(By.CSS_SELECTOR, "._ab8y._ab94._ab97._ab9f._ab9k._ab9p._abcm")
         for element in elements:
-            if element.text not in el:
+            if element.text not in el and "Verified" not in element.text:
                 el.append(element.text)
-                t = 0
-
     except:
-        pass
+        raise Exception
 
-for i in el:
-    print(i)
-print(len(el))
+with open('followers.txt', 'a') as d:
+    for i in el:
+        d.write(i+"\n")
+
+with open('followers.txt', 'r') as d:
+    old_data = d.readlines()
+    print([i.replace('\n', '') for i in old_data])
+    print([i.replace('\n', '') for i in el])
+    for j in [i.replace('\n', '') for i in old_data]:
+        if j not in [i.replace('\n', '') for i in el]:
+            print(ј + " unfollowed you from Instagram")
+
+with open("followers.txt", 'w') as d:
+    for i in el:
+        d.write(i + '\n')
 
 time.sleep(500)
 driver.close()
